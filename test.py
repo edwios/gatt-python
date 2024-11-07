b964e677cf02b180a625dc4bcfc955d030c28005
Edwin Tam <edwin.tam@telldus.se>
2024-11-07 14:43:09 +0100

    Test config update

--- test.py
+++ test.py
@@ -3,13 +3,14 @@ from gatt import DeviceManager
 if __name__ == "__main__":
     # Example usage of DeviceManager
 
-    host_name = "TELLDUS_030000"
+    host_name = "TELLDUS_GATT"
     mqtt_host = "mqtt.telldus.com"
-    mqtt_port = 8883  # Typically 8883 for MQTT over TLS
+    mqtt_port = 30042  # Typically 8883 for MQTT over TLS
     mqtt_user = "TELLDUS_030000"
     mqtt_password = "qM9KXFw3Dkpt"
+    target_host_name = "TELLDUS_E87F95" # who are we listening to and communicating with
 
-    manager = DeviceManager(host_name, mqtt_host, mqtt_port, mqtt_user, mqtt_password)
+    manager = DeviceManager(host_name, mqtt_host, mqtt_port, mqtt_user, mqtt_password, target_host_name)
 
     # Start discovery for specific device names
     device_names = ["uRskKI4BUh"]
